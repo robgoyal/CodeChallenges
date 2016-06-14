611b589166a96c484f6876d8499f5b20e6c7a635
robgoyal <robin_goyal@hotmail.com>
2016-06-14 12:48:18 -0400

    first commit

---
+++ 30DaysOfCode/HelloWorld.cpp
@@ -0,0 +1,20 @@
+/* Created by Robin Goyal, June 12, 2016. */
+/* Introductory Hello World Program */
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string inputStr;
+    getline(cin, inputStr);
+
+    cout << "Hello, World." << endl;
+    cout << inputStr << "\n";
+
+    return 0;
+}
