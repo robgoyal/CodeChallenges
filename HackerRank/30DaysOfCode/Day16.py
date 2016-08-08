88013f244b2397550fe66ea300dd7d43100e12bd
robgoyal <robin_goyal@hotmail.com>
2016-08-07 22:16:46 -0400

    Day16 Challenge Completed

---
+++ HackerRank/30DaysOfCode/Day16.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env/python
+
+# Day16.py
+# Code written by Robin Goyal
+# Created on 07-08-2016
+# Last updated on 07-08-2016
+
+strInput = raw_input().strip()
+
+try:
+    print int(strInput)
+
+except:
+    print "Bad String"
